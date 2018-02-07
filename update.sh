6d1ddaba49d5751e480bbe041b17b7c6269280cf
Weston Steimel <weston.steimel@gmail.com>
2018-02-07 00:47:51 +0000

    Add generated files for alpine 3.4 and 3.5.
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ sed_escape_rhs() {
 	echo "$@" | sed -e 's/[\/&]/\\&/g' | sed -e ':a;N;$!ba;s/\n/\\n/g'
 }
 
-alpine_versions=(3.6 3.7)
+alpine_versions=(3.4 3.5 3.6 3.7)
 
 for version in "${versions[@]}"; do
     echo "Generating Dockerfiles for Boost version ${version}."
