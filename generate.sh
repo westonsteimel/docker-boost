fab3b95f98c37031faa32b1fae460974eef5747d
Weston Steimel <weston.steimel@gmail.com>
2018-06-21 10:15:15 +0100

    Check the checksum of the download
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- generate.sh
+++ generate.sh
@@ -16,11 +16,14 @@ sed_escape_rhs() {
 }
 
 alpine_versions=(3.6 3.7)
+declare -A boost_checksums
+boost_checksums=(["1.67.0"]="8aa4e330c870ef50a896634c931adf468b21f8a69b77007e45c444151229f665" ["1.66.0"]="bd0df411efd9a585e5a2212275f8762079fed8842264954675a4fddc46cfcf60")
 
 for version in "${versions[@]}"; do
     echo "Generating Dockerfiles for Boost version ${version}."
     template=alpine
     boost_url_dir=boost_${version//./_}
+    checksum=${boost_checksums[$version]}
     echo "Generating templates for ${template}"
 
     for alpine_version in ${alpine_versions[@]}; do
@@ -29,6 +32,7 @@ for version in "${versions[@]}"; do
 	sed -r \
 	    -e 's!%%TAG%%!'"$alpine_version"'!g' \
 	    -e 's!%%BOOST_VERSION%%!'"$version"'!g' \
+	    -e 's!%%BOOST_CHECKSUM%%!'"$checksum"'!g' \
 	    -e 's!%%BOOST_URL_DIR%%!'"$boost_url_dir"'!g' \
             "Dockerfile-${template}.template" > "$version/$template/$alpine_version/Dockerfile"
 	echo "Generated ${version}/${template}/${alpine_version}/Dockerfile"
